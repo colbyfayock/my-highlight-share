9c9b232284df0d8edae20d3a3f43fc2a9d1d76c6
Colby Fayock <fayock@gmail.com>
2024-02-11 12:04:51 -0500

    Step 05: Dynamically positioning tooltip over selected text

--- src/components/SelectMenu.tsx
+++ src/components/SelectMenu.tsx
@@ -3,10 +3,28 @@ import { Twitter } from 'lucide-react';
 
 const SelectMenu = () => {
   const [selection, setSelection] = useState<string>();
+  const [position, setPosition] = useState<Record<string, number>>();
 
   useEffect(() => {
     document.addEventListener('selectionchange', () => {
-      setSelection(document?.getSelection()?.toString());
+      const activeSelection = document.getSelection();
+      const text = activeSelection?.toString();
+
+      if ( !activeSelection || !text ) {
+        setSelection(undefined);
+        return;
+      };
+
+      setSelection(text);
+
+      const rect = activeSelection.getRangeAt(0).getBoundingClientRect()
+
+      setPosition({
+        x: rect.left + (rect.width / 2) - (80 / 2),
+        y: rect.top + window.scrollY - 30,
+        width: rect.width,
+        height: rect.height,
+      })
     });
   }, []);
 
@@ -22,12 +40,15 @@ const SelectMenu = () => {
 
   return (
     <div role="dialog" aria-labelledby="share" aria-haspopup="dialog">
-      {selection && (
+      {selection && position && (
         <p
           className="
-            relative w-[80px] h-[30px] bg-black text-white rounded m-0
+            absolute -top-2 left-0 w-[80px] h-[30px] bg-black text-white rounded m-0
             after:absolute after:top-full after:left-1/2 after:-translate-x-2 after:h-0 after:w-0 after:border-x-[6px] after:border-x-transparent after:border-b-[8px] after:border-b-black after:rotate-180
           "
+          style={{
+            transform: `translate3d(${position.x}px, ${position.y}px, 0)`
+          }}
         >
           <button
             className="flex w-full h-full justify-between items-center px-2"
